b58b31b1a5d80167c17242f34e991044b540b2ce
chipmunk-dev <devquokkajeong@gmail.com>
2023-01-30 23:58:17 +0900

    learn global object & console object

---
+++ 2-console/app.js
@@ -0,0 +1,28 @@
+console.log("logging....");
+console.clear();
+
+// log level
+console.log("log"); // 개발
+console.info("info"); // 정보
+console.warn("warn"); // 경보
+console.error("error"); // 에러, 사용자 에러, 시스템 에러
+
+console.assert(2 === 3, "not same!");
+console.assert(2 === 2, "same!");
+
+const student = { name: "quokka", age: 30, compony: { name: "AC" } };
+console.log(student);
+console.table(student);
+console.dir(student);
+
+console.time("for liip");
+for (let i = 0; i < 1000; i++) {}
+console.timeEnd("for liip");
+
+function a() {
+	console.count("a function");
+}
+
+a();
+console.countReset("a function");
+a();
