04e3fee3263e0382269aa4124b1f64edd756e901
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-03 23:55:48 +0900

    learn events module

--- 13-event/app.js
+++ 13-event/app.js
@@ -1,14 +1,17 @@
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
-emitter.on("quokka", args => {
-	console.log("first callback -> ", args);
-});
+const callback = args => {
+	console.log("first callback - ", args);
+};
+
+emitter.on("quokka", callback);
 
 emitter.on("quokka", args => {
-	console.log("first callback -> ", args);
+	console.log("second callback - ", args);
 });
 
 emitter.emit("quokka", { message: 1 });
 emitter.emit("quokka", { message: 2 });
+emitter.removeAllListeners();
 emitter.emit("quokka", { message: 3 });
