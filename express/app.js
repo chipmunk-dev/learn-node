3a8189fab80b6dd39db169f8a16a10ee8ead1324
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-19 00:06:26 +0900

    learn middleware

--- express/app.js
+++ express/app.js
@@ -2,14 +2,37 @@ import express from "express";
 
 const app = express();
 
-app.get("/sky/:id", (req, res, next) => {
-	// console.log(req.path);
-	// console.log(req.headers);
-	console.log(req.params);
-	console.log(req.params.id);
-	console.log(req.query);
-	console.log(req.query.keyword);
-	res.send("hi!");
+app.all("/api", (req, res, next) => {
+	console.log("all");
+	next();
+});
+app.use("/sky", (req, res, next) => {
+	console.log("use");
+	next();
+});
+
+app.get(
+	"/",
+	(req, res, next) => {
+		console.log("first");
+		res.send("Hello");
+	},
+	(req, res, next) => {
+		console.log("first2");
+		next();
+	}
+);
+
+app.get("/", (req, res, next) => {
+	console.log("second");
+});
+
+app.use((req, res, next) => {
+	res.status(404).send("Not avilable!");
+});
+app.use((error, req, res, next) => {
+	console.error(error);
+	res.status(500).send("Sorry, try later!");
 });
 
 app.listen(8080);
