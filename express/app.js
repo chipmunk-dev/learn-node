da379959777f483bce5693c2f1667c1e307329ef
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-19 00:35:32 +0900

    learn error handling

--- express/app.js
+++ express/app.js
@@ -1,38 +1,29 @@
 import express from "express";
+import fs from "fs";
 
 const app = express();
 
-app.all("/api", (req, res, next) => {
-	console.log("all");
-	next();
-});
-app.use("/sky", (req, res, next) => {
-	console.log("use");
-	next();
+app.use(express.json());
+
+app.get("/file", (req, res) => {
+	fs.readFile("/file1.txt", (err, data) => {
+		if (err) {
+			res.sendStatus(404);
+		}
+	});
 });
 
-app.get(
-	"/",
-	(req, res, next) => {
-		console.log("first");
-		res.send("Hello");
-	},
-	(req, res, next) => {
-		console.log("first2");
-		next();
+app.get("/file1", (req, res) => {
+	try {
+		const data = fs.readFileSync("/file1.txt");
+		res.send(data);
+	} catch (error) {
+		res.sendStatus(404);
 	}
-);
-
-app.get("/", (req, res, next) => {
-	console.log("second");
 });
 
-app.use((req, res, next) => {
-	res.status(404).send("Not avilable!");
-});
-app.use((error, req, res, next) => {
-	console.error(error);
-	res.status(500).send("Sorry, try later!");
+app.post("/", (req, res, next) => {
+	console.log(req.body);
 });
 
 app.listen(8080);
